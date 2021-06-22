b6fa40c6a3fa4a77cfb00c1182bfd701f7f80e30
Salvador <salvadorludovico.sl@gmail.com>
2021-06-22 00:08:37 -0300

    DAY-2: db, migration, user creation added

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,13 @@
-import express from "express"; // ... significa que as tipagems existem mas não está dentro delas (há uma biblioteca a parte com as tipagens)
+import "reflect-metadata";
+import express from "express";
+import { router } from "./routes";
 
-// @types/express -> yarn add @types/express -D
+import "./database";
+
 const app = express();
 
+app.use(express.json());
 
-/**
- * GET    => Buscar uma informação (listagens, ...)
- * POST   => Inserir uma informação (criações, ...)
- * PUT    => Alterar uma informação (manipulação de um dado já existente)
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação específica
- */
-app.get("/test", (request, response) => {
-    // Request  => Entrada
-    // Response => Saída
-    return response.send("Olá NLW");
-})
+app.use(router);
 
-app.post("/test-post", (request, response) => {
-    return response.send("método POST");
-})
-
-// http://localhost:3000
 app.listen(3000, () => { console.log("Server is running") });
